6cf76d093d1774f2616b21e3f65a92b13e2d0171
Chenfeng Su <Cfengsu2002@gmail.com>
2026-03-31 17:50:43 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        course_schedule=defaultdict(list)
+        for i in range(len(prerequisites)):
+            course_schedule[prerequisites[i][0]].append(prerequisites[i][1])
+        valid_course=set()
+        is_valid=True
+        current_taking=set()
+        def dfs(current_course):
+            nonlocal valid_course, is_valid
+            if(current_course in current_taking):
+                is_valid=False
+                return
+            if(current_course in valid_course):
+                return
+            if(course_schedule[current_course]==[]):
+                valid_course.add(current_course)
+                return 
+            current_taking.add(current_course)
+            for element in course_schedule[current_course]:
+                dfs(element)
+            current_taking.remove(current_course)
+            valid_course.add(current_course)
+            return 
+        for i in range(numCourses):
+            dfs(i)
+        return is_valid
+            
+            
