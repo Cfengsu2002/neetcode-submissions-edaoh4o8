52acf2618e560e976ba490ae4ac6b869be0abf8f
Chenfeng Su <Cfengsu2002@gmail.com>
2026-04-30 18:34:34 -0400

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        current_prev_course={}
+        for course in range(numCourses):   
+            current_prev_course[course]=[]
+        for current_course, prev_course in prerequisites:
+            current_prev_course[current_course].append(prev_course)
+        
+        allowed_course=set()
+        currently_taking=set()
+        def dfs(current_course):
+            nonlocal currently_taking, allowed_course
+            if(current_course in allowed_course):
+                return True
+            if(current_prev_course[current_course]==[]):
+                allowed_course.add(current_course)
+                return True
+
+            currently_taking.add(current_course)
+            for prev_course in current_prev_course[current_course]:
+                if(prev_course in currently_taking):
+                    return False
+                if not dfs(prev_course):
+                    return False
+            currently_taking.remove(current_course)
+            return True
+        
+        
+        
+        
+        
+        for current_course in range(numCourses):
+            if not dfs(current_course):
+                return False
+        return True
+        print(current_prev_course)
+
